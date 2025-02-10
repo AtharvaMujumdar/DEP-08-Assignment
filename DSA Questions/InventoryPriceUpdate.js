791af7e7d124151931c02da19f0d2589bdf2dc95
AtharvaEpam <atharvamukul_mujumdar@epam.com>
2025-02-10 16:08:49 +0530

    modify solution

--- DSA Questions/InventoryPriceUpdate.js
+++ DSA Questions/InventoryPriceUpdate.js
@@ -1,7 +1,7 @@
 const products = [   { id: 1, name: "Widget", price: 50, quantity: 120 },   { id: 2, name: "Gadget", price: 30, quantity: 80 },   { id: 3, name: "Doohickey", price: 100, quantity: 200 },   { id: 4, name: "Thingamajig", price: 75, quantity: 90 } ];
 
 const products1 = products.map((obj) =>{
-    if(obj.price > 100){
+    if(obj.quantity > 100){
         return {...obj, price:0.9*obj.price};
     }
     return obj;
